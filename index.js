0de6b7a911e86674e05a9fe7756109dc5ca91bda
marcio8queiroz <marcio8queiroz@hotmail.com>
2024-02-21 11:48:05 -0400

    adicionando um item no corpo da requisição

--- index.js
+++ index.js
@@ -19,8 +19,36 @@ const lista = ['Rick Sanchez', 'Morty Smith', 'Sumer Smith']
 
 // Read All -> [GET] / item
 app.get('/item', function (req, res) {
+  // Envio a lista inteira como resposta HTTP
   res.send(lista)
 })
 
+//Read By ID -> [GET] /item/:
+app.get('/item/:id', function (req, res) {
+  //Acesso o id no parâmetro de rota 
+  const id = req.params.id
+
+  // Acesso item na lista baseado no ID recebido
+  const item = lista[id]
+
+  // Envio o item obtido como resposta HTTP
+  res.send(item)
+})
+
+app.use(express.json())
+
+//Create - > [POST]/item
+app.post('/item', function (req, res) {
+  //Extraímos o corpo da requisição
+  const body = req.body
+// pegamos o nome que foi enviado dentro do corpo
+  const item = body.nome
+
+  //Colocamos o nome dentro da lista de itens
+  lista.push(item)
+
+ res.send('Item adicionado com sucesso')
+ 
+})
  
 app.listen(3000)
