689115dd055aa764cbaf50b0d05a8c8627d1795a
marcio8queiroz <marcio8queiroz@hotmail.com>
2024-02-21 10:24:50 -0400

    endpoint lista

--- index.js
+++ index.js
@@ -6,12 +6,20 @@ const app = express()
 
 app.get('/', function (req, res) {
 
-  res.send('hello world')
+  res.send('hello, world!')
 
   app.get('/oi', function (req, res) {
     res.send('Olá mundo!')
   })
 
+})
+
+//Lista de pesonagens
+const lista = ['Rick Sanchez', 'Morty Smith', 'Sumer Smith']
+
+// Read All -> [GET] / item
+app.get('/item', function (req, res) {
+  res.send(lista)
 })
 
  
